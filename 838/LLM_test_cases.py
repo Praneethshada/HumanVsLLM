0df4cbfea1116594417643aed63282ef7f50735b
QasimANaik <93054498+QasimANaik@users.noreply.github.com>
2025-12-02 11:32:32 +0530

    Delete 838 directory

--- 838/LLM_test_cases.py
+++
@@ -1,6 +0,0 @@
-from buggy_function import min_Swaps
-
-def test_cases():
-    assert min_Swaps("0011","1111") == 1
-    assert min_Swaps("00011","01001") == 2
-    assert min_Swaps("111","111") == 0
