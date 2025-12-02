839ae8dfdc93c30609c660e2cf92a1c893049450
QasimANaik <93054498+QasimANaik@users.noreply.github.com>
2025-12-02 11:32:08 +0530

    Delete 853 directory

--- 853/LLM_test_cases.py
+++
@@ -1,6 +0,0 @@
-from buggy_function import sum_of_odd_Factors
-
-def test_examples():
-    assert sum_of_odd_Factors(30) == 24
-    assert sum_of_odd_Factors(18) == 13
-    assert sum_of_odd_Factors(2) == 1
